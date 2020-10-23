33c4698fff4c2ef718f5d91865e3174c984cb35d
ArifurRahmanUAP <49741745+ArifurRahmanUAP@users.noreply.github.com>
2020-10-23 19:03:20 +0600

    Car-Game

---
+++ Assets/CarController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CarController : MonoBehaviour
+{
+	public enum Axel
+{
+    Front,
+    Rear
+}
+
+[Serializable]
+public struct Wheel
+{
+    public GameObject model;
+    public WheelCollider collider;
+    public Axel axel;
+}
+     [SerializeField]
+	 private float speed= 0.0f;
+    [SerializeField]
+    private float maxAcceleration = 15.0f;
+    [SerializeField]
+    private float turnSensitivity = 1.0f;
+    [SerializeField]
+    private float maxSteerAngle = 45.0f;
+    [SerializeField]
+    private Vector3 _centerOfMass;
+    [SerializeField]
+    private List<Wheel> wheels;
+
+    private float inputX, inputY=0.0f;
+
+    private Rigidbody _rb;
+
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.centerOfMass = _centerOfMass;
+    }
+
+    
+    private void Update()
+    {
+		speed+=.01f*Time.deltaTime;
+        AnimateWheels();
+        GetInputs();
+		inputY +=speed*Time.deltaTime;
+		Move();
+        Turn();
+    }
+
+    private void GetInputs()
+    {
+        inputX = Input.GetAxis("Horizontal");
+    }
+
+    private void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.collider.motorTorque = inputY * maxAcceleration * 500 * Time.deltaTime;
+        }
+    }
+
+    private void Turn()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                var _steerAngle = inputX * turnSensitivity * maxSteerAngle;
+                wheel.collider.steerAngle = Mathf.Lerp(wheel.collider.steerAngle,_steerAngle,0.5f);
+            }
+        }
+    }
+
+    private void AnimateWheels()
+    {
+        foreach (var wheel in wheels)
+        {
+            Quaternion _rot;
+            Vector3 _pos;
+            wheel.collider.GetWorldPose(out _pos, out _rot);
+            wheel.model.transform.position = _pos;
+            wheel.model.transform.rotation = _rot;
+        }
+    }
+}
---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollision : MonoBehaviour
+{
+	public CarController movement;
+    public GameObject car;
+	public 	float flag=1f;
+    void Update()
+    {
+    }
+
+
+    void OnCollisionEnter(Collision hit)
+    {
+        
+        if (hit.collider.tag == "tree")
+        {
+			flag=0;
+            movement.enabled=false;
+			Destroy(car);
+        }
+		
+    }
+}
